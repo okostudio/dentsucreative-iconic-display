4fa73d75e32f2db41d22511c48c6b0eb4574f6fb
okostudio <marek.suchanski@gmail.com>
2025-04-16 12:29:06 +1000

    iframes

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <div class="screenshot">
+    <div class="banners">
+      <iframe class="banner mrec" width="300" height="250" src="/post-event-videos-300x250/index.html"></iframe>
+      <iframe class="banner halfpage" width="300" height="600" src="/post-event-videos-300x600/index.html"></iframe>
+    </div> 
   </div>
 `
 
