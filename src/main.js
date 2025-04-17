fa2c969b877569e75b635c667735f94916b2c2c9
okostudio <marek.suchanski@gmail.com>
2025-04-17 15:12:21 +1000

    loop

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ document.querySelector('#app').innerHTML = `
 <div class="warning">PLEASE VIEW WITHOUT ADBLOCKING ENABLED :)</div>
   <div class="screenshot">
     <div class="banners">
-      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.jpg">
+      <video class="banner mrec" width="300" height="250" autoplay muted loop poster="/iconic-300x250.jpg">
         <source src="/iconic-300x250.webm" type="video/webm">
       </video>
-      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.jpg">
+      <video class="banner halfpage" width="300" height="600" autoplay muted loop poster="/iconic-300x600.jpg">
         <source src="/iconic-300x600.webm" type="video/webm">
       </video>
     </div> 
