2578042f9ed81e87a278880eb3738fe6d10dfcbe
okostudio <marek.suchanski@gmail.com>
2025-04-17 15:01:20 +1000

    manual play videos

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div class="screenshot">
     <div class="banners">
-      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.jpg">
+      <video class="banner mrec" width="300" height="250" controls autoplay muted poster="/iconic-300x250.jpg">
         <source src="/iconic-300x250.webm" type="video/webm">
       </video>
       <video class="banner halfpage" width="300" height="600" controls poster="/iconic-300x600.jpg">
-        <source src="/iconic-300x600.webm" type="video/webm">
-      </video>
-      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.jpg">
         <source src="/iconic-300x600.webm" type="video/webm">
       </video>
     </div> 
