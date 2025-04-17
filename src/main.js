2d84f933d285bb9034e7f6c20de9050caf91cfd1
okostudio <marek.suchanski@gmail.com>
2025-04-17 15:07:24 +1000

    reverted to OG... adblocker message added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
+<div class="warning">PLEASE VIEW WITHOUT ADBLOCKING ENABLED :)</div>
   <div class="screenshot">
     <div class="banners">
-      <video class="banner mrec" width="300" height="250" controls autoplay muted poster="/iconic-300x250.jpg">
+      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.jpg">
         <source src="/iconic-300x250.webm" type="video/webm">
       </video>
-      <video class="banner halfpage" width="300" height="600" controls poster="/iconic-300x600.jpg">
+      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.jpg">
         <source src="/iconic-300x600.webm" type="video/webm">
       </video>
     </div> 
