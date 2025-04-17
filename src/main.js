c742da46225612b046ff3ba34b535b2224d4c53f
okostudio <marek.suchanski@gmail.com>
2025-04-17 14:56:09 +1000

    posters added

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div class="screenshot">
     <div class="banners">
-      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.webm">
+      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.jpg">
         <source src="/iconic-300x250.webm" type="video/webm">
       </video>
-      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.webm">
+      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.jpg">
         <source src="/iconic-300x600.webm" type="video/webm">
       </video>
     </div> 
