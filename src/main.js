ff2e7a67e6d84fd173809843ddaccf2224730b74
okostudio <marek.suchanski@gmail.com>
2025-04-17 14:47:14 +1000

    background saturation + video posteres

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div class="screenshot">
     <div class="banners">
-      <video class="banner mrec" width="300" height="250" autoplay muted>
+      <video class="banner mrec" width="300" height="250" autoplay muted poster="/iconic-300x250.webm">
         <source src="/iconic-300x250.webm" type="video/webm">
       </video>
-      <video class="banner halfpage" width="300" height="600" autoplay muted>
+      <video class="banner halfpage" width="300" height="600" autoplay muted poster="/iconic-300x600.webm">
         <source src="/iconic-300x600.webm" type="video/webm">
       </video>
     </div> 
